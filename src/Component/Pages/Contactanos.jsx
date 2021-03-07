8596d2dbd6116c6e9f577a866bed1bc6b3106cfe
steven poveda <HugoStevenPoveda@hotmail.com>
2021-03-07 13:41:30 -0500

    commit de form google

--- src/Component/Pages/Contactanos.jsx
+++ src/Component/Pages/Contactanos.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Contactanos=()=>(
     <>
-
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfP31VFjgom6sEGO1GWvI2f_EVvse17pTNR7AaUUEIk-5DdDQ/viewform?embedded=true" width="640" height="955" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfKUvASgcapuICJHbVN6M4Z6wqhE_Nd91LCUZZvVtrSpqDzCA/viewform?embedded=true" width="1000" height="983" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+      
     </>
 
 
